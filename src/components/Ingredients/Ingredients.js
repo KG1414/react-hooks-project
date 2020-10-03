4ac7b7acaf58a0615dd974e1589c1a9ee39a2fb0
kylegallard <kylegallard@gmail.com>
2020-10-03 17:01:44 +1000

    useEffect in use to persist data after re-render

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
@@ -7,19 +7,38 @@ import Search from './Search';
 function Ingredients() {
   const [userIngredients, setUserIngredients] = useState([]);
 
+  useEffect(() => {
+    fetch('https://react-hooks-update-7a7a9.firebaseio.com/ingredients.json')
+      .then(response => response.json())
+      .then(responseData => {     // responseData will be an object from Firebase, not an array
+        const loadedIngredients = [];
+        for (const key in responseData) {
+          loadedIngredients.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount
+          });
+        }
+        setUserIngredients(loadedIngredients);
+      });
+  }, []); //only re-renders if dependencies (in this array) change, avoiding an infinity loop
+
+
+
   const addIngredientHandler = ingredient => {
     fetch('https://react-hooks-update-7a7a9.firebaseio.com/ingredients.json', {
       method: 'POST',
       body: JSON.stringify(ingredient),
       headers: { 'Content-Type': 'application/json' }
     }).then(response => {
+      return response.json();     // to handle data as JSON object
+    }).then(responseData => {
       setUserIngredients(prevIngredients => [
         ...prevIngredients,
-        { id: Math.random().toString(), ...ingredient }
-
+        { id: responseData.name, ...ingredient }
       ]);
     });
-  }
+  };
 
   const removeIngredientHandler = ingredientId => {
     setUserIngredients((prevIngredients) => prevIngredients.filter((ingredient) => ingredient.id !== ingredientId));
